23430d057545f54f638ade85a7e3b0ff8fd3d9da
EC2 Default User <ec2-user@ip-172-31-39-125.ap-northeast-1.compute.internal>
2020-04-11 09:48:19 +0000

    add_validates_to_user_model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,13 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
+  before_save { self.email = email.downcase if email }
   validates :name,  presence: true, length: { maximum: 50 }
-  validates :email, presence: true, length: { maximum: 255 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
   has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
   
   
   
--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -1,44 +1,58 @@
 require 'rails_helper'
 
-describe 'ユーザーアカウント機能', type: :system do
-  describe 'ユーザー登録機能' do
-    context do
-      before do
-        visit signup_path
-        fill_in 'ユーザー名', with: 'テストユーザー'
-        fill_in 'メールアドレス', with: 'a@example.com'
-        fill_in 'パスワード', with: 'password'
-        fill_in 'パスワード（確認）', with: 'password'
-      end
-      
-      it 'ユーザー登録成功' do
-        expect{ click_button '登録' }.to change{ User.count }.by(1)
-      end
-      
-      it 'ユーザー登録失敗（ユーザー名が空欄）' do
-        fill_in 'ユーザー名', with: ''
-        expect{ click_button '登録' }.not_to change{ User.count }
-      end
-      
-      it 'ユーザー登録失敗（メールアドレスが空欄)' do
-        fill_in 'ユーザー名', with: ''
-        expect{ click_button '登録' }.not_to change{ User.count }
-      end
-      
-      it 'ユーザー登録失敗（ユーザー名が長い）' do
-        fill_in 'ユーザー名', with: "a"*51
-        expect{ click_button '登録' }.not_to change{ User.count }
-      end
-      
-      it 'ユーザー登録失敗（メールアドレスが長い)' do
-        fill_in 'ユーザー名', with: "a"*234+"@example.com"
-        expect{ click_button '登録' }.not_to change{ User.count }
-      end
-      
-      # it 'ユーザー登録失敗（メールアドレスが不適）' do
-      #   fill_in 'ユーザー名', with: 'aaaaaaa'
-      #   expect{ click_button '登録' }.not_to change{ User.count }
-      # end
-    end
+describe 'ユーザー登録機能', type: :system do
+  before do
+    visit signup_path
+    fill_in 'ユーザー名', with: 'テストユーザー'
+    fill_in 'メールアドレス', with: 'test1@example.com'
+    fill_in 'パスワード', with: 'password'
+    fill_in 'パスワード（確認）', with: 'password'
   end
-end
+
+  it 'ユーザー登録成功' do
+    expect{ click_button '登録' }.to change{ User.count }.by(1)
+  end
+
+  context 'ユーザー登録失敗' do
+
+    it 'ユーザー名が空欄' do
+      fill_in 'ユーザー名', with: ''
+      expect{ click_button '登録' }.not_to change{ User.count }
+    end
+
+    it 'メールアドレスが空欄' do
+      fill_in 'メールアドレス', with: ''
+      expect{ click_button '登録' }.not_to change{ User.count }
+    end
+
+    it 'ユーザー名が長い' do
+      fill_in 'ユーザー名', with: "a"*51
+      expect{ click_button '登録' }.not_to change{ User.count }
+    end
+
+    it 'メールアドレスが長い' do
+      fill_in 'メールアドレス', with: "a"*244+"@example.com"
+      expect{ click_button '登録' }.not_to change{ User.count }
+    end
+
+    it 'メールアドレスが不適' do
+      fill_in 'メールアドレス', with: 'aaaaaaa'
+      expect{ click_button '登録' }.not_to change{ User.count }
+    end
+
+    it 'メールアドレスが重複' do
+      FactoryBot.create(:user)
+      expect{ click_button '登録' }.not_to change{ User.count }
+    end
+
+    it 'パスワードが空欄' do
+      fill_in 'パスワード', with: ''
+      expect{ click_button '登録' }.not_to change{ User.count }
+    end
+
+    it 'パスワードが短い' do
+      fill_in 'パスワード', with: 'aaaaa'
+      expect{ click_button '登録' }.not_to change{ User.count }
+    end
+  end  
+end 
