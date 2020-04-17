04afe34949fc1a543b42a910f77c309f48120959
Miki Yudai <miki_yudai_19891111@yahoo.co.jp>
2020-04-17 13:39:22 +0000

    Postのrequest specを追記

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,17 +1,21 @@
 class PostsController < ApplicationController
-  before_action :logged_in_user, only: [:create, :destroy]
+  before_action :logged_in_user, only: [:new, :create, :destroy]
   before_action :correct_user, only: [:edit, :update, :destroy]
   
   def index
-    redirect_to current_user
+    if logged_in?
+      redirect_to current_user
+    else
+      redirect_to root_url
+    end
   end
   
   def new
-    @post = Post.new(post_params)
+    @post = Post.new
   end
   
   def show
-      @post = Post.find_by(id: params[:id])
+    @post = Post.find_by(id: params[:id])
   end
   
   def create
--- spec/request/posts_spec.rb
+++ spec/request/posts_spec.rb
@@ -1,6 +1,78 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts controller', type: :request do
+  let!(:user) { FactoryBot.create(:user) }
 
+  describe 'GET #index' do
+    context 'ログインしていない場合' do
+      before do
+        get posts_path
+      end
+      
+      it 'リクエストが成功する' do
+        expect(response.status).to eq 302
+      end
+      
+      it 'root_urlにリダイレクトされること' do
+        expect(response).to redirect_to root_url
+      end
+    end
+    
+    context 'ログインしている場合' do
+      before do
+        post login_path, params: { session: { email: user.email,
+                                      password: user.password,
+                                      remember_me: '1'} }
+        get posts_path
+      end
+      
+      it 'リクエストが成功する' do
+        expect(response.status).to eq 302
+      end
+      
+      it 'current_userにリダイレクトされること' do
+        expect(response).to redirect_to user_path(user)
+      end
+    end
+  end
+  
+  describe 'GET #new' do
+    context 'ログインしていない場合' do
+      before do
+        get new_post_path
+      end
+      
+      it 'リクエストが成功する' do
+        expect(response.status).to eq 302
+      end
+      
+      it 'logiin_pathにリダイレクトされること' do
+        expect(response).to redirect_to login_url
+      end
+    end
+    
+    context 'ログインしている場合' do
+      before do
+        post login_path, params: { session: { email: user.email,
+                                      password: user.password,
+                                      remember_me: '1'} }
+        get posts_path
+      end
+      
+      it 'リクエストが成功する' do
+        expect(response).to redirect_to user_path(user)
+      end
+    end
+  end
+  
+  describe 'GET #show' do
+    it 'リクエストが成功する' do
+      post login_path, params: { session: { email: user.email,
+                                    password: user.password,
+                                    remember_me: '1'} }
+      get post_path(user.posts.first)
+      expect(response.status).to eq 200
+    end
+  end
 
 end
