7d7996e1053c7f23d301e0acef999fadc71e85f6
Miki Yudai <miki_yudai_19891111@yahoo.co.jp>
2020-04-19 04:37:18 +0000

    posts_controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
-  before_action :logged_in_user, only: [:new, :create, :destroy]
-  before_action :correct_user, only: [:create, :update, :destroy]
+  before_action :logged_in_user, only: [:new, :create, :update, :destroy]
+  before_action :correct_user, only: [:new, :create, :update, :destroy]
   
   def index
     if logged_in?
