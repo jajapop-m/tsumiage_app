664fc5fa06e20cf3f85b6f65a0c262c2e8c15910
Miki Yudai <miki_yudai_19891111@yahoo.co.jp>
2020-04-21 05:25:41 +0000

    activationテストを追記

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -4,7 +4,7 @@ class AccountActivationsController < ApplicationController
     if user && !user.activated? && user.authenticated?(:activation, params[:id])
       user.activate
       log_in user
-      flash[:success] = "メールアドレスを確認が完了しました。"
+      flash[:success] = "メールアドレスの確認が完了しました。"
       redirect_to user_path(user)
     else
       flash[:danger] = "無効なリンクです。"
