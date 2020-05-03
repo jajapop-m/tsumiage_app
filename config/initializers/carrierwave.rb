1f2d6355bc3456e4cfc06fb66629894786897074
Miki Yudai <miki_yudai_19891111@yahoo.co.jp>
2020-05-03 13:56:02 +0000

    heroku 設定

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,13 +1,14 @@
 CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
 
 if Rails.env.production?
- CarrierWave.configure do |config|
-  config.fog_credentials = {
-   :provider              => 'AWS',
-   :region                => Credentials.env.dig(:S3_REGION),
-   :aws_access_key_id     => Credentials.env.dig(:S3_ACCESS_KEY),
-   :aws_secret_access_key => Credentials.env.dig(:S3_SECRET_KEY)
-  }
-  config.fog_directory    =  Credentials.env.dig(:S3_BUCKET)
- end
+  CarrierWave.configure do |config|
+    config.fog_credentials = {
+      # Amazon S3用の設定
+      :provider              => 'AWS',
+      :region                => ENV['S3_REGION'],
+      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key => ENV['S3_SECRET_KEY']
+    }
+    config.fog_directory     =  ENV['S3_BUCKET']
+  end
 end
