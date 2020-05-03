ad55c4172a19838f3e6ea9de0fc33552b0d057f4
Miki Yudai <miki_yudai_19891111@yahoo.co.jp>
2020-05-03 15:08:49 +0000

    send grid修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'tsumiage-app.herokuapp.com'
+  host = 'tsumi-age.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
